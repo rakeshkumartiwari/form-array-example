56fe9cf31a77acc2b0f254d15e9811990b1f37aa
rakeshkumartiwari <tiwarirakesh8@gmail.com>
2020-04-12 03:50:50 +0530

    form design done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  stateList = [
+    { id: 101, name: 'Maharashtra' },
+    { id: 102, name: 'Telangana' },
+    { id: 103, name: 'Odisha' }
+  ];
+
   title = 'form-array-example';
+  addressGroup: FormGroup;
+  constructor(private fb: FormBuilder) { }
+
+  ngOnInit() {
+    this.addressGroup = this.fb.group({
+      address1: ['', Validators.required],
+      address2: ['', Validators.required],
+      state: ['', Validators.required],
+      city: ['', Validators.required],
+      zip: [null, Validators.maxLength(6)]
+    });
+  }
+
 }
+
