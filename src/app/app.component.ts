c9a7bdea6647ee32c711a1de9c32746a4616426b
rakeshkumartiwari <tiwarirakesh8@gmail.com>
2020-04-12 15:43:16 +0530

    implemented formarray

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators, FormArray } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -15,16 +15,31 @@ export class AppComponent implements OnInit {
 
   title = 'form-array-example';
   addressGroup: FormGroup;
+
+  get addresses(): FormArray {
+    return this.addressGroup.get('addresses') as FormArray;
+  }
+
   constructor(private fb: FormBuilder) { }
 
   ngOnInit() {
     this.addressGroup = this.fb.group({
+      addresses: this.fb.array([this.buildAddress()])
+    });
+  }
+
+  buildAddress(): FormGroup {
+    return this.fb.group({
       address1: ['', Validators.required],
       address2: ['', Validators.required],
       state: ['', Validators.required],
       city: ['', Validators.required],
       zip: [null, Validators.maxLength(6)]
     });
+  }
+
+  addAddress() {
+    this.addresses.push(this.buildAddress());
   }
 
 }
